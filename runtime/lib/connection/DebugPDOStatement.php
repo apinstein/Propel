d912c8b8bb5052e5ef5fdb327f64bc9f7e676b44
Alan Pinstein <apinstein@mac.com>
2012-01-18 17:55:49 -0500

    [gh-118] Update full query logging support to log actual SQL in case of error in DebugPDOStatement::execute, PropelPDO::exec, and PropelPDO::query.
    - Add PropelPDOException to allow propel to customize PDOExceptions
      without causing BC problems for code that expects PDOException.
    - Include full SQL in PropelPDOException so you can view the full SQL
      which caused the error without having to look in the log file.

--- runtime/lib/connection/DebugPDOStatement.php
+++ runtime/lib/connection/DebugPDOStatement.php
@@ -99,12 +99,23 @@ class DebugPDOStatement extends PDOStatement
     public function execute($input_parameters = null)
     {
         $debug = $this->pdo->getDebugSnapshot();
+
+		$exception = null;
+		try {
         $return = parent::execute($input_parameters);
+		} catch (Exception $e) {
+			$exception = $e;
+		}
 
         $sql = $this->getExecutedQueryString($input_parameters?:array());
         $this->pdo->log($sql, null, __METHOD__, $debug);
         $this->pdo->setLastExecutedQuery($sql);
         $this->pdo->incrementQueryCount();
+
+		if ($exception)
+		{
+			throw new PropelPDOException("Unable to execute SQL:\n{$sql}.", $exception);
+		}
 
         return $return;
     }
